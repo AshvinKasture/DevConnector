13d51f22069a608bd538172a9ce8f04ac28c996a
Ashvin Kasture <kashvin525@gmail.com>
2022-01-13 22:41:24 +0530

    sync 13-01-22_10:41 pm

--- models/Post.js
+++ models/Post.js
@@ -1,52 +1,54 @@
-const mongoose, {Schema} = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-    user: {
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
+      },
     },
-    text: {
-        type: String, required: true
-    },
-    name: {
-        type: String
-    },
-    avatar: {
-        type:String
-    },
-    likes: [
-        {
-           user: {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        }
-    ],
-    comments: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            },
-            text: {
-                type: String
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ],
-    date: {
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      text: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = Post = mongoose.model('Post', PostSchema);
